bb4132fb27c35f0c2fa8af6e5ec22898143f69e0
Danish-Ali097 <danishali20021996@gmail.com>
2024-05-08 19:48:49 +0500

    working on login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,57 @@
 import React, { useState } from 'react'
+import Input from './components/Input/Input';
 import { useGoogleLogin } from '@react-oauth/google'
-import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons/faGoogle'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram'
+import { faApple } from '@fortawesome/free-brands-svg-icons/faApple'
+import Switch from './components/Switch/Switch';
 
 function App() {
   const [data, setData] = useState({
+    isEmailLogin: false,
+    code: "",
+    phone: "",
     email: "",
     password: ""
   })
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const { name, value } = e.currentTarget
-    setData((preval) => ({
-      ...preval,
-      [name]: value
-    }))
+    const { name, value, checked } = e.currentTarget
+    if(e.currentTarget.type == 'checkbox') {
+      setData((preval) => ({
+        ...preval,
+        [name]: checked
+      }))
+    } else {
+      setData((preval) => ({
+        ...preval,
+        [name]: value
+      }))
+    }
   }
 
   const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     let headers = new Headers();
     headers.append('Authorization', 'Basic ' + btoa("modl_app_user:6qfxSkl9yO"))
-    headers.append('Content-Type','application/json')
-    let options = { 
+    headers.append('Content-Type', 'application/json')
+    let options = {
       method: 'POST',
       headers,
       body: JSON.stringify({
         "isEmailLogin": true,
-        "code": "+92",
-        "phone": "3065877851",
+        "code": data.code,
+        "phone": data.phone,
         "email": data.email,
         "password": data.password
-      }) 
+      })
     };
     fetch("https://localhost:44363/api/User/Login", options)
-    .then(r => {
-      console.log(r.json());
-    })
-    .catch(e => console.error(e));
+      .then(r => {
+        console.log(r.json());
+      })
+      .catch(e => console.error(e));
   }
 
   const handleGoogleLogin = useGoogleLogin({
@@ -54,40 +69,38 @@ function App() {
 
   return (
     <>
-      <div className='p-8'>
-        <form className="w-full max-w-sm" onSubmit={handleSignIn}>
-          <div className="md:flex md:items-center mb-6">
-            <div className="md:w-1/3">
-              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
-                Email/Phone
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input value={data.email} onChange={handleChange} name='email' className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="test@example.com"></input>
-            </div>
-          </div>
-          <div className="md:flex md:items-center mb-6">
-            <div className="md:w-1/3">
-              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password">
-                Password
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input value={data.password} onChange={handleChange} name='password' className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************"></input>
-            </div>
-          </div>
+      <div className='p-8 w-full'>
+        <form className="w-full max-w-sm mx-auto shadow-md rounded py-8 px-4" onSubmit={handleSignIn}>
+          <Switch name="isEmailLogin" checkedLabel='' unCheckedLabel='' checked={data.isEmailLogin} onChange={handleChange} />
+          {
+            data.isEmailLogin ?
+              <Input id="email" type="email" placeholder="john@example.com" name="email" value={data.email} onChange={handleChange} label="Email" />
+              :
+              <div>
+                <Input id="code" type="text" placeholder="+xx" name="code" value={data.code} onChange={handleChange} label="Code" />
+                <Input id="phone" type="phone" placeholder="3xxxxxxxxx" name="phone" value={data.phone} onChange={handleChange} label="Phone" />
+              </div>
+          }
+
+          <Input id="password" type="password" placeholder="*************" name="password" value={data.password} onChange={handleChange} label="Password" />
           <div className="text-center">
-            <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3" type="submit">
+            <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3 w-full" type="submit">
               Sign Up/Login
             </button>
-            <hr/>
-            <button onClick={handleGoogleLogin} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3" type="button">
+            <hr />
+            <button onClick={handleGoogleLogin} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3 w-3/4" type="button">
+              <FontAwesomeIcon icon={faGoogle} />
+              &nbsp;
               Google
             </button>
-            <button onClick={handleInstaLogin} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3" type="button">
+            <button onClick={handleInstaLogin} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3 w-3/4" type="button">
+              <FontAwesomeIcon icon={faInstagram} />
+              &nbsp;
               Instagram
             </button>
-            <button onClick={handleAppleLogin} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3" type="button">
+            <button onClick={handleAppleLogin} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3 w-3/4" type="button">
+              <FontAwesomeIcon icon={faApple} />
+              &nbsp;
               Apple
             </button>
           </div>
