594128426d5109c08c96642d1490340fd01df0b2
Danish-Ali097 <danishali20021996@gmail.com>
2024-05-08 19:52:21 +0500

    login form fix

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ function App() {
           <Input id="password" type="password" placeholder="*************" name="password" value={data.password} onChange={handleChange} label="Password" />
           <div className="text-center">
             <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3 w-full" type="submit">
-              Sign Up/Login
+              Login
             </button>
             <hr />
             <button onClick={handleGoogleLogin} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded block m-auto my-3 w-3/4" type="button">
--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -7,13 +7,13 @@ interface ISwitch extends InputHTMLAttributes<HTMLElement> {
     onChange: ChangeEventHandler<HTMLInputElement>
 }
 
-function Switch({ checkedLabel, unCheckedLabel, ...rest }:ISwitch) {
+function Switch({ checkedLabel, unCheckedLabel, ...rest }: ISwitch) {
     return (
         <>
             <label className="relative inline-flex items-center cursor-pointer mb-3 left-6">
                 <input className="sr-only peer" {...rest} type="checkbox"></input>
-                    <div className="peer rounded-full outline-none duration-100 after:duration-500 w-80 h-10 bg-blue-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-purple-500  after:content-['Phone'] after:absolute after:outline-none after:rounded-full after:h-8 after:w-40 after:bg-white after:top-1 after:left-2 after:flex after:justify-center after:items-center  after:text-sky-800 after:font-bold peer-checked:after:translate-x-36 peer-checked:after:content-['Email'] peer-checked:after:border-white">
-                    </div>
+                <div className="peer rounded-full outline-none duration-100 after:duration-500 w-80 h-10 bg-blue-300 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-purple-500  after:content-['Phone'] after:absolute after:outline-none after:rounded-full after:h-8 after:w-40 after:bg-white after:top-1 after:left-2 after:flex after:justify-center after:items-center  after:text-sky-800 after:font-bold peer-checked:after:translate-x-36 peer-checked:after:content-['Email'] peer-checked:after:border-white">
+                </div>
             </label>
         </>
     )
